17ec621682d835de78596aeecfb8e72971b49502
investormide21 <156132044+investormide21@users.noreply.github.com>
2024-01-09 23:13:42 +0100

    Add files via upload

---
+++ _next/static/I-NQ0J3h5wHbKiEvtm_Hw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/505-bd99de2d7212d87d.js","static/css/8eebd7bb6fc92c93.css","static/chunks/pages/index-2d80caed88cd6728.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
